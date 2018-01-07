e462fcbfd39a36034bb08b68e95fab3ef6ff4406
Denise Pendleton <dpendl1@hotmail.com>
2018-01-07 17:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten() {
+      kittens.push("Ralph")
+      return kittens
+}
+function destructivelyPrependKitten() {
+      kittens.unshift("Bob")
+      return kittens
+}
+function destructivelyRemoveLastKitten() {
+     
+      kittens.pop()
+      return kittens
+}
+function destructivelyRemoveFirstKitten() {
+     
+      kittens.shift()
+      return kittens
+}
+function appendKitten() {
+     var kittens2 = [...kittens, "Broom"]
+      return kittens2
+}
+
+
+
+
+
+
+
+
+
+
+
